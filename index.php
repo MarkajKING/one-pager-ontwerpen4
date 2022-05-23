34959449b748fffa81c4a02e8637ca6f39a476d8
Mark Hobbel <89836879+MarkajKING@users.noreply.github.com>
2022-05-23 15:43:04 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,169 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+    <title>Brandaan™</title>
+
+    <link rel="icon" href="https://www.burkunk.nl/wp-content/uploads/2015/11/03Monniken_ridders.png"/>
+    <link rel="stylesheet" href="style.css?v=<?= time(); ?>"/>
+</head>
+
+<body>
+<nav>
+    <div><a href="#opening">Inleiding</a></div>
+    <div><a href="#doelgroep">Doelgroep</a></div>
+    <div><a href="#mechanics">Mechanics</a></div>
+    <div><a href="#coreDrives">Core-Drives</a></div>
+    <div><a href="#artStyle">Art-Style</a></div>
+</nav>
+
+<header></header>
+
+<main>
+    <section id="opening">
+        <h2>
+            Welkom!
+        </h2>
+        <p>
+            Op deze website vind je alle informatie over de game Brandaan en de tijdmachine!
+        </p>
+    </section>
+
+    <section id="doelgroep">
+        <h2>
+            De doelgroep
+        </h2>
+        <p>
+            Onze doelgroep zijn kinderen uit groep 5. Hierbij past de leeftijd 8-9 jaar. Hierdoor zijn de leeskunsten
+            nog niet hoog ontwikkeld, en daarom moet onze tekst niet moeilijk zijn. Het moet kort en makkelijk zijn, met
+            een duidelijke boodschap. In 1 zin moet als het ware de hele boodschap duidelijk zijn. Dit is ook te zien in
+            het geschiedenis boek wat wij hebben.
+        </p>
+        <p>
+            Kinderen uit groep 5 hebben niet heel lang een focus en raken ook snel afgeleid. Daarom moeten we de game
+            veel intereacite geven, zodat ze de focus kunnen blijven houden. Ook moeten we zorgen dat er constant iets
+            te doen is en dat er steeds directe feedback uit de game komt. Zo kunnen deze kinderen zich van 10 tot 15
+            minuten concentreren. <a href="(https://www.menselijklichaam.nl/psychologie/concentratie/)">(https://www.menselijklichaam.nl/psychologie/concentratie/)</a>
+        </p>
+        <p>
+            Een goede manier waarop we op deze concentratieboog kunnen aansluiten is dat we ervoor kunnen zorgen dat de
+            game opgeslagen kan worden en op een ander moment verder gespeeld kan worden.
+        </p>
+        <p>
+            Kinderen uit groep 5 hebben waarschijnlijk nog niet zo veel ervaring met gaming op een laptop. Deze
+            generatie zal het wel iets meer hebben dan dat ik dat had, maar ik denk toch dat we duidelijk een intro
+            moeten maken op onze game, zodat ook de mensen met weinig ervaring het kunnen spelen. Ook als ze te lang
+            vast zitten kunnen ze extra uitleg vragen. Zo kunnen ze altijd verder.
+        </p>
+        <p>
+            Daarnaast maken wij de game voor op de pc/laptop, omdat deze op scholen het meest beschikbaar zijn en het
+            makkelijkst in gebruik. Kinderen hebben vaak hier ook al ervaring mee.
+        </p>
+    </section>
+
+    <section id="mechanics">
+        <h2>
+            De mechanics
+        </h2>
+
+        <p>
+            De speler heeft zich uiteraard te houden aan een aantal regels. Deze regels zijn er om de ervaring van de
+            game te verbeteren en de speler te helpen met het spelletje. De regels zijn als volgt:
+        </p>
+        <p>
+            De speler mag het speelveld niet verlaten, dit doen we omdat de speler niet verdwaald mag raken tijdens dit
+            spelletje. Stel dit zou kunnen kan dit ten koste gaan van de al kleine concentratieboog. <br>
+            De speler moet opdrachten uitvoeren om zijn tijdmachine te maken. Hij moet met verschillende mensen praten,
+            en voor deze mensen dan ook opdrachten uitvoeren zodat hij de onderdelen van zijn tijdmachine weer kan
+            maken. Tijdens deze opdrachten komt dan de stof voorbij die de leerlingen moeten leren. Zo leert het kind
+            spelenderwijs de benodigde stof uit het hoofdstuk. <br>
+            Verder kan de speler wel vrij rondlopen, en wordt dit niet voor hem gedaan, zodat de speler nog wel enige
+            vorm van vrijheid ervaart tijdens het spelen van dit spel.
+        </p>
+        <div>
+            <img
+                    class="picture"
+                    src="./img/fish.png"
+                    alt="hardware">
+        </div>
+    </section>
+
+    <section id="coreDrives">
+        <h2>
+            Core-Drives
+        </h2>
+
+        <p>
+            Mijn code voor de Giftbox™ is vrij simpel. Zoals je hieronder op het
+            plaatje ziet heb ik variabelen aangemaakt en if-statements gebruikt.
+            De variabelen samen met de if-statements zorgen ervoor dat de Giftbox™
+            de opdracht niet meerdere keren achter elkaar doet. Hierdoor blijft
+            het doosje normaal open en dicht gaan, en wordt de melodie niet
+            verstoord.
+        </p>
+
+        <div>
+            <img
+                    class="picture"
+                    src="https://thumbor.pijper.io/OiAHZT8C2ie3mkBL3kmQvcOx7W8=/980x0/https://cdn.pijper.io/2021/12/Fq6yviZojfZTvh1639498216.jpg"
+                    alt="giftbox"
+            />
+        </div>
+    </section>
+    <section id="artStyle">
+        <h2>
+            Art Style
+        </h2>
+
+        <p>
+            Voor de art-style hebben we gekozen voor een 'The legend of Zelda: A link to the past'
+        </p>
+        <div>
+            <img
+                    class="picture"
+                    src="https://www.zeldadungeon.net/Zelda03/Walkthrough/02/007.png"
+                    alt="hardware">
+        </div>
+        <p>
+            Zoals je ziet is het een 2d style, wat ook de bedoeling was voor onze game. Alles is dezelfde stijl en
+            pixelachtig. Het zijn schattige afbeeldingen die passen bij de kinderen.
+        </p>
+        <div>
+            <img
+                    class="picture"
+                    src="./img/allPacks.png"
+                    alt="hardware">
+        </div>
+        <div>
+            <img
+                    class="picture"
+                    src="./img/People.png"
+                    alt="hardware">
+        </div>
+        <p>
+            We hebben ook gekozen voor zachte kleuren, die soepel in elkaar overlopen zodat de kinderen niet overprikkeld raken. 
+        </p>
+    </section>
+</main>
+
+<footer>
+    <p>
+        <a
+                href="https://www.instagram.com/programming_by_mark/"
+                target="_blank"
+        >
+            <img
+                    id="footerImage"
+                    src="https://cdn.glitch.me/b8be9af3-ed38-4b0e-8435-74c6f577d123%2Finstagram-logo-vrijstaand.png?v=1633678813767"
+                    alt="instaLogo"
+            />
+            Programming by mark
+        </a>
+    </p>
+</footer>
+</body>
+</html>
